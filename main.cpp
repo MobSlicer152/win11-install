f64d60b77487fc5b6e3b85fb8ca554a84e4eb533
MobSlicer152 <brambleclaw1414@gmail.com>
2023-07-21 11:21:42 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <windows.h>
+
+int main()
+{
+    std::cout << "Windows 11 Installer for Unsupported Devices" << std::endl <<
+        "Rewrite of https://github.com/Mr-Unknown-74/Windows-11-Installer-for-Incompatible-Devices by Mr. Unknown_74" << std::endl;
+
+    
+}
+
