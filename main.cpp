f688199ec999476f65a590b410ad80ebee3be768
MobSlicer152 <brambleclaw1414@gmail.com>
2023-07-21 12:09:11 -0400

    Add precompiled header

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
-#include <iostream>
+#include "stdafx.h"
 
-#include <windows.h>
+bool g_isPhase2;
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "Windows 11 Installer for Unsupported Devices" << std::endl <<
-        "Rewrite of https://github.com/Mr-Unknown-74/Windows-11-Installer-for-Incompatible-Devices by Mr. Unknown_74" << std::endl;
+        "Rewrite of https://github.com/Mr-Unknown-74/Windows-11-Installer-for-Incompatible-Devices by my comrade Mr. Unknown_74" << std::endl
+#ifdef Debug
+        << "WIP version" << std::endl
+#endif
+        ;
 
-    
+    std::cout << "Press any key to exit";
+    (void)_getch();
 }
 
---
+++ stdafx.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <conio.h>
+#include <windows.h>
